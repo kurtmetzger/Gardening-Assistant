12ff6e8d47d459670416d8a5244d62760233dd5b
kmetzger <108840734+kurtmetzger@users.noreply.github.com>
2025-08-20 20:24:10 -0500

    Moved addPlants sorting logic to route

--- routes/addPlants.js
+++ routes/addPlants.js
@@ -22,10 +22,33 @@ router.get('/addPlants', async (req, res) => {
       //formats today's date to compare to plant ranges
       const date = new Date();
       const formattedDate = `${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+
+      //Converts into array for sorting
+        let plantArray = Object.entries(fullPlantingData[zone] || {});
+
+        if (req.user.addPlantsSort === 'date') {
+        //Redefines built in sort method logic by explaining how a and b should be compared.
+            plantArray.sort((a, b) => {
+            //Gets date from element a, then date from element b in sorting. Adds '-' if no data exists for spring planting
+            const aDate = a[1].plantOutdoorsStart || "-";
+            const bDate = b[1].plantOutdoorsStart || "-";
+            // Puts plants without spring planting dates (Garlic) at the bottom because negative results go later
+            if (aDate === "-") return 1;
+            if (bDate === "-") return -1;
+            //Seperates by month and day and converts into comparable numbers
+            const aMonth = Number(aDate.split("-")[0])
+            const aDay = Number(aDate.split("-")[1])
+            const bMonth = Number(bDate.split("-")[0])
+            const bDay = Number(bDate.split("-")[1])
+            //If months are different, return difference. Otherwise, return difference of days
+            if (aMonth !== bMonth) return aMonth - bMonth;
+            return aDay - bDay;
+            });
+        }
   
   
       const zoneData = fullPlantingData[zone];
-      res.render('addPlants', { user: req.user, plantingData: zoneData, dateToday: formattedDate, addedUpcoming: addedUpcoming, addedPlanted: addedPlanted });
+      res.render('addPlants', { user: req.user, plantingData: plantArray, dateToday: formattedDate, addedUpcoming: addedUpcoming, addedPlanted: addedPlanted });
     } catch(err) {
       console.error(err);
       res.status(500).send('Error loading full planting dates in route');
