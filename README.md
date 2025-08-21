f4d169b5927da0a4bf07946ed492d637b8120654
kmetzger <108840734+kurtmetzger@users.noreply.github.com>
2025-08-20 20:41:05 -0500

    Moved some javascript from the views into the routes

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 This is a web application that assists in managing planting and harvesting dates for home gardeners. Some tasks this application assists in include:
 * Allowing users to keep a list of all plants they are planning on growing for the season
 * Keeping track of optimal haarvesting and planting dates
-* Adjusting planting dates depending on planting zone
+* Adjusting planting dates depending on planting zone of user
 * Allowing users to explore plants and seeds in in season to plant on current day
-* Allowing users to keep a list of all plants they are planning on growing for the season
+
+This repo also includes a version of a webscraper I made to get this data used for the project from almanac.com called almanacWebScraper.py, as well as the design artifacts created during the planning stages of this application
--- routes/myGarden.js
+++ routes/myGarden.js
@@ -19,10 +19,36 @@ router.get('/', async (req, res) => {
         //formats today's date to compare to plant ranges
         const date = new Date();
         const formattedDate = `${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+        
+            //Same code as add plants page, but just grabs items where they are in upcoming
+        let upcomingArray = req.user.userGarden.filter(plant => plant.datePlanted === "0");
+
+        if (req.user.upcomingSort === 'date') {
+        //Redefines built in sort method logic by explaining how a and b should be compared.
+            upcomingArray.sort((a, b) => {
+            //Gets date from element a, then date from element b in sorting. Adds '-' if no data exists for spring planting
+            const aDate = fullPlantingData[zone][a.name].plantOutdoorsStart || "-";
+            const bDate = fullPlantingData[zone][b.name].plantOutdoorsStart || "-";
+            // Puts plants without spring planting dates (Garlic) at the bottom because negative results go later
+            if (aDate === "-") return 1;
+            if (bDate === "-") return -1;
+            //Seperates by month and day and converts into comparable numbers
+            const aMonth = Number(aDate.split("-")[0])
+            const aDay = Number(aDate.split("-")[1])
+            const bMonth = Number(bDate.split("-")[0])
+            const bDay = Number(bDate.split("-")[1])
+            //If months are different, return difference. Otherwise, return difference of days
+            if (aMonth !== bMonth) return aMonth - bMonth;
+            return aDay - bDay;
+            });
+        } else{
+            //This one needs to be sorted alphabetically, but the master plants list is automatically
+            upcomingArray.sort((a, b) => a.name.localeCompare(b.name));
+        }
     
     
         const zoneData = fullPlantingData[zone];
-        res.render('myGarden', { user: req.user, plantingData: zoneData, dateToday: formattedDate });
+        res.render('myGarden', { user: req.user, plantingData: zoneData, upcomingArray: upcomingArray, dateToday: formattedDate });
       } catch(err) {
         console.error(err);
         res.status(500).send('Error loading myGarden page');
